5f05c240f15a929e8230d98afbdb35120cc777c0
Elcio10 <donelli@live.nl>
2016-10-27 14:41:59 +0200

    C++

---
+++ WinMain.cpp
@@ -0,0 +1,9 @@
+/* 
+ * File:   WinMain.cpp
+ * Author: emendes
+ *
+ * Created on 27 oktober 2016, 13:56
+ */
+
+#include "WinMain.h"
+
---
+++ WinMain.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   WinMain.h
+ * Author: emendes
+ *
+ * Created on 27 oktober 2016, 14:22
+ */
+
+#ifndef WINMAIN_H
+#define	WINMAIN_H
+
+
+
+#endif	/* WINMAIN_H */
+ 
